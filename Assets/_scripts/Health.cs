30c35ee8b46f7662212689805437af0bf71c58d8
Ambidexteria <dvoeyko@gmail.com>
2024-05-14 07:25:46 +0300

    Updtae_2
    Added assets packages:
    1) 2D Pixel Art Platformer Biome - American Forest
    2) Dragon Warrior Files
    3) Miniature Army 2D V.1
    Also completed Health.cs

--- Assets/_scripts/Health.cs
+++ Assets/_scripts/Health.cs
@@ -1,18 +1,31 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private float _maxValue;
+
     public event Action Dying;
+
+    public float Current { get; private set; }
 
     public void Increase(float amount)
     {
+        if(amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount) + " in " + nameof(Health));
 
+        Current += amount;
+        Current = Mathf.Clamp(Current, 0, _maxValue);
     }
 
     public void Decrease(float amount)
     {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount) + " in " + nameof(Health));
+        
+        Current -= amount;
 
+        if (Current <= 0)
+            Dying?.Invoke();
     }
 }
